77c7e3db433a0e96673cf5ad2a2f27b9f3738293
Foster Stule <camodude5@yahoo.com>
2018-05-24 12:06:44 -0400

    made some data to populate the table

--- user table.sql
+++ user table.sql
@@ -1,5 +1,5 @@
-drop table user
-create table user
+drop table [user]
+create table [User]
 	(
 		Id int not null identity(1,1) primary key,
 		Username nvarchar(30) not null unique,
@@ -12,3 +12,32 @@ create table user
 		IsAdmin bit not null default 0,
 		Active bit  not null default 1
 	);
+
+insert into [User]
+	(Username,Password,Firstname,Lastname,phone,email, IsAdmin,IsReviewer)
+values
+	('propofthedead','Helper108*','Foster','Stulen','513-459-5655','camodude5@yahoo.com',1,0)
+
+	insert into [User]
+	(Username,Password,Firstname,Lastname,phone,email)
+values
+	('rararasputin','drowned','Rasputin','Russia','513-961,6565','rarara@aol.com')
+insert into [User]
+	(Username,Password,Firstname,Lastname,phone,email)
+values
+	('satan666','lucifer','Lucifier','Angel','666-666-1234', 'satanrulez@hell.com')
+insert into [User]
+	(Username,Password,Firstname,Lastname,phone,email)
+values
+	('traceOw','itsinthebag','Lena','Oxton','514-564-4865','heyaimbritish@overwatch.com')
+insert into [User]
+	(Username,Password,Firstname,Lastname,phone,email,IsAdmin,IsReviewer)
+values
+	('bush420','blazeit','George','Bush','111-111-1111','highpresident@whitehouse.yournotsupposetoknow',1,0)
+
+insert into [User]
+	(Username,Password,Firstname,Lastname,phone,email,IsAdmin,IsReviewer)
+values
+	('maxgreg','training','Greg','Doud','513-487-9545','gregdoud@maxTrain.com',1,0)
+
+	select * from [User]
